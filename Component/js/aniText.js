3d545d342698fd75543f2b24f3b2d6e83f6666de
Sam tao <xutaog24@gmail.com>
2015-09-09 16:00:43 +0800

    marquee

---
+++ Component/js/aniText.js
@@ -0,0 +1,26 @@
+﻿var scPanel = document.getElementById("scPanel");
+	var pan1 = document.getElementById("pan1");
+	var pan2 = document.getElementById("pan2");
+	var listHight = 34;
+	pan2.innerHTML = pan1.innerHTML;
+	scTime = 0;
+	var speed = 50,delay = 2000;
+	function  startMove(){
+		scPanel.scrollTop++;
+		scTime = setInterval(scrollTop,speed);
+	}
+	
+	function scrollTop(){
+		var offTop = pan1.offsetHeight;
+		if(scPanel.scrollTop%listHight ==0){
+			clearTimeout(scTime);
+			setTimeout(startMove,delay);
+		}else{
+			if(scPanel.scrollTop>=offTop){
+				scPanel.scrollTop = 0;
+			}else{
+				scPanel.scrollTop++;
+			}
+		}
+	}
+	setTimeout(startMove,delay);
